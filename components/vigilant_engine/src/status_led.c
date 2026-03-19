fbdc7f5090bf422ace85ff239a22b105a7a663eb
Matthias Grabner <FJ76583946278@protonmail.com>
2026-03-19 16:39:49 +0100

    add invert led functionality

--- components/vigilant_engine/src/status_led.c
+++ components/vigilant_engine/src/status_led.c
@@ -7,6 +7,12 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 
+#if defined(CONFIG_VE_INVERT_STATUS_LED)
+    #define INVERT_LED 1
+#else
+    #define INVERT_LED 0
+#endif
+
 /*
 Choose mode depending on board or preference
 
@@ -17,36 +23,25 @@ BLINK mode:
 RGB mode:
 - GREEN = Info 
 - BLUE  = Warning
-- RED  = ERROR
+- RED   = Error
 */
-
-#if defined(VE_STATUS_LED_MODE_RGB)
-    #define CURRENT_MODE 0
-#else
-    #define CURRENT_MODE 1
-#endif
 
 typedef enum {
     MODE_RGB,
     MODE_BLINK
 } led_mode_t;
 
-#if defined(CONFIG_VE_STATUS_LED_MODE_RGB)
-    #define CURRENT_LED_MODE MODE_RGB
-#else
-    #define CURRENT_LED_MODE MODE_BLINK
-#endif
-
 static TaskHandle_t s_blink_task = NULL;
-
-static void blink_led(uint8_t led_gpio)
-{
-    // Set the GPIO level according to the state (LOW or HIGH)
-    gpio_set_level(led_gpio, s_blink.state);
-}
+uint8_t led_on = 1;
+uint8_t led_off = 0;
 
 void configure_led()
 {
+    if (INVERT_LED) {
+        led_on = 0;
+        led_off = 1;
+    }
+
 #if defined(CONFIG_VE_STATUS_LED_MODE_RGB)
     gpio_reset_pin(CONFIG_VE_STATUS_LED_GPIO_RED);
     gpio_reset_pin(CONFIG_VE_STATUS_LED_GPIO_GREEN);
@@ -60,6 +55,12 @@ void configure_led()
     gpio_reset_pin(CONFIG_VE_STATUS_LED_GPIO_BLINK);
     gpio_set_direction(CONFIG_VE_STATUS_LED_GPIO_BLINK, GPIO_MODE_OUTPUT);
 #endif
+}
+
+static void blink_led(uint8_t led_gpio)
+{
+    // Set the GPIO level according to the state (LOW or HIGH)
+    gpio_set_level(led_gpio, s_blink.state);
 }
 
 static void blink_task(void *arg)
@@ -104,16 +105,16 @@ esp_err_t status_led_set_state(status_state_t state) {
 #if defined(CONFIG_VE_STATUS_LED_MODE_RGB)
     switch (state) {
         case STATUS_STATE_INFO:
-            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_RED, 1);
-            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_BLUE, 1);
+            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_RED, led_off);
+            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_BLUE, led_off);
             return status_led_blink_start(1000, 1000, CONFIG_VE_STATUS_LED_GPIO_GREEN);
         case STATUS_STATE_WARNING:
-            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_GREEN, 1);
-            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_RED, 1);
+            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_GREEN, led_off);
+            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_RED, led_off);
             return status_led_blink_start(600, 600, CONFIG_VE_STATUS_LED_GPIO_BLUE);
         case STATUS_STATE_ERROR:
-            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_GREEN, 1);
-            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_BLUE, 1);
+            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_GREEN, led_off);
+            gpio_set_level(CONFIG_VE_STATUS_LED_GPIO_BLUE, led_off);
             return status_led_blink_start(300, 300, CONFIG_VE_STATUS_LED_GPIO_RED);
         default:
             return status_led_blink_stop();
